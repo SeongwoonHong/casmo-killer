4811d6e58a893b39ede1d7c5d60258d16d1f9769
seongwoon hong <saytoyou91@gmail.com>
2017-10-30 23:04:45 -0400

    initial commit

---
+++ client/src/actions/types.js
@@ -0,0 +1,1 @@
+export const TOGGLE_MAINMENU = 'TOGGLE_MAINMENU';
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-import { BrowserRouter } from 'react-router-dom';
+import reducers from './reducers';
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-    <BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
         <App/>
-    </BrowserRouter>,
+      </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
 

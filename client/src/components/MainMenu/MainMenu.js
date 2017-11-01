f69ad8c7738def483d0494ec4cb16dfb5b6fd359
goginim <david.ingook@gmail.com>
2017-11-01 11:44:47 -0400

    airbnb

--- client/src/components/MainMenu/MainMenu.js
+++ client/src/components/MainMenu/MainMenu.js
@@ -9,14 +9,19 @@ const mainMenuItems = [
   { name: 'KILLING', link: '/test3', icon: 'email' },
   { name: 'CASMO', link: '/test4', icon: 'shopping_cart' },
   { name: 'HAHAHA', link: '/test5', icon: 'search' }
-]
+];
 class MainMenu extends Component {
   render() {
     return (
-      <div id="main-menu" className={classnames({ 'expanded': this.props.menu.isExpanded })}>
-        <div onClick={ this.props.toggleMenu }>
+      <div id="main-menu" className={classnames({ expanded: this.props.menu.isExpanded })}>
+        <div
+          onClick={this.props.toggleMenu}
+          role="button"
+          tabIndex={0}
+          onKeyDown={this.props.toggleMenu}
+        >
           <div className="logo">
-            <span></span>
+            <span />
           </div>
           <p className="menu-text">MENU</p>
         </div>
@@ -24,12 +29,14 @@ class MainMenu extends Component {
         <ul className="menu-item">
           {
             mainMenuItems.map((menu) => {
-              return <li key={menu.name}>
-                      <Link to={`${this.props.menu.isExpanded ? menu.link : '#'}`}>
-                        <i className="material-icons">{ menu.icon }</i>
-                        <p className="menu-item-text">{ menu.name }</p>
-                      </Link>
-                    </li>
+              return (
+                <li key={menu.name}>
+                  <Link to={`${this.props.menu.isExpanded ? menu.link : '#'}`}>
+                    <i className="material-icons">{ menu.icon }</i>
+                    <p className="menu-item-text">{ menu.name }</p>
+                  </Link>
+                </li>
+              );
             })
           }
         </ul>

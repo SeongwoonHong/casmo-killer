7704924f6c2cdf8531895abe10c0c4652caadb43
seongwoon hong <saytoyou91@gmail.com>
2017-11-10 19:56:19 -0500

    created login and button testing routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';

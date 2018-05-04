cbc9a4e8acae0fbe6bdb2f7146df32f541e8a400
lyndonoc <ssinsoo@gmail.com>
2018-05-04 15:48:44 -0400

    email switched to sendgrid, email template complete

--- client/src/index.js
+++ client/src/index.js
@@ -11,15 +11,14 @@ import registerServiceWorker from './registerServiceWorker';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-const configureStore = () => createStore(
+const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(reduxThunk)
 );
 
-const store = configureStore();
-
 let TestRoutes;
+
 if (isDevelopment) {
   TestRoutes = require('./test/routes').default;
 }

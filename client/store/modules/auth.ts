9482b8f81433b084bb8e526dcf415b2cfe2a0128
lyndonoc <lyndonoc@gmail.com>
2019-08-23 01:03:52 -0400

    basic auth setup

--- client/store/modules/auth.ts
+++ client/store/modules/auth.ts
@@ -19,44 +19,49 @@ export const login = (email: string, password: string) => {
       });
       const res = await axios.post('/auth/local/login', {
         email,
-        password
+        password,
+      }, {
+        withCredentials: true,
       });
+      console.log(`USER DATA\n`, res.data.user);
 
       dispatch({
         type: LOGIN_SUCCESS,
         payload: res.data,
-      })
+      });
     } catch (e) {
       console.log(e);
       dispatch({ type: LOGIN_FAIL });
     }
-  }
-}
+  };
+};
 
 export const requestSignup = (email: string) => {
-  return axios.post('/auth/local/register', {
-    email
-  })
+  return () => {
+    // axios.post('/auth/local/register', {
+    //   email
+    // })
+  };
 };
 
 export const signup = ({
   email,
   password,
   displayName,
-  avatar = null
+  avatar = null,
 }) => {
   return async (dispatch) => {
     try {
       dispatch({
         type: SIGNUP,
-      })
+      });
 
       const res = await axios.post('/auth/local/register', {
         email,
         password,
         displayName,
         avatar,
-      })
+      });
 
       dispatch({
         type: SIGNUP_SUCCESS,
@@ -65,55 +70,62 @@ export const signup = ({
           displayName,
           userId: 1,
         },
-      })
+      });
     } catch (e) {
       console.log(e);
       dispatch({
         type: SIGNUP_FAIL,
-      })
+      });
     }
-  }
-}
+  };
+};
 
 export const initialize = () => {
   return async () => {
     try {
-      const res = await axios.get('/token/csrf');
+      const res = await axios.get('/token/csrf', {
+        withCredentials: true,
+      });
       const csrfToken = res.headers['x-csrf-token'];
+
+      console.log('/token/csrf returns new csrf token\n', csrfToken);
 
       if (csrfToken) {
         setTokenToHeader('x-csrf-token', csrfToken);
       }
+      // return res;
+      // save this data somewhere
+      // ...
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-  }
+  };
 };
 
 export const tokenRefresh = () => {
   return async () => {
     try {
-      const cookies = new Cookies();
-      const token = cookies.get('DAMSO_AUTH_TOKEN');
+      const res = await axios.post(
+        '/token/refresh',
+        {},
+        {
+          withCredentials: true,
+        },
+      );
 
-      if (!token) {
-        return;
-      }
+      console.log('/tokens/refresh returns new user data\n', res.data.user);
+      console.log('/tokens/refresh returns new access token\n', res.headers['x-auth-token']);
 
-      setTokenToHeader('x-auth-token', token);
-
-      const res = await axios.post('/token/refresh');
-      console.log('tokenRefresh response = ', res);
     } catch (e) {
       console.log(e);
     }
-  }
-}
+  };
+};
 
 const initialState = {
   user: null,
   isLoading: false,
-}
+};
 
 /* REDUCER */
 export default (state = initialState, action) => {
@@ -149,7 +161,7 @@ export default (state = initialState, action) => {
       return {
         ...state,
         isLoading: false,
-      }
+      };
     default:
       return state;
   }
